e7e2c7d8608236ac55d299f5c4f935f1a1f29e4e
SankRea <1370316409@qq.com>
2026-09-08 16:35:41 +0800

    Site updated: 2026-09-08 16:35:24

---
+++ js/crash_cheat.js
@@ -0,0 +1,14 @@
+var OriginTitle = document.title;
+var titleTime;
+
+document.addEventListener('visibilitychange', function() {
+    if (document.hidden) {
+        document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
+        clearTimeout(titleTime);
+    } else {
+        document.title = '(ฅ>ω<*ฅ) 噫又好了~';
+        titleTime = setTimeout(function() {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
